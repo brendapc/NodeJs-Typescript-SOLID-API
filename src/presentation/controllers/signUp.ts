5cea31c50e9329c2df24d2ae3a68682c75e67a7c
Brenda Pereira <brendacamara@outlook.com>
2021-02-12 11:10:38 -0300

    refactor: make signupController interface protocol

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -1,11 +1,17 @@
+import { Controller } from "./../protocols/controller";
 import { MissingParamError } from "./../protocols/errors/missing-param-error";
 import { badRequest } from "./../helpers/http-helper";
 import { HttpRequest, HttpResponse } from "./../protocols/http";
 
-export class SignUpController {
+export class SignUpController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-    for(const field of requiredFields){
+    const requiredFields = [
+      "name",
+      "email",
+      "password",
+      "passwordConfirmation",
+    ];
+    for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
       }
