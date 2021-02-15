eeec955884fa524df4ae68fec3582bd1d7b8a87c
Brenda Pereira <brendacamara@outlook.com>
2021-02-15 16:12:49 -0300

    test: ensure EmailValidatorAdapter call email validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,18 +1,17 @@
-import { EmailValidatorAdapter } from './email-validator';
-import validator from 'validator'
-import { isMainThread } from 'worker_threads';
+import { EmailValidatorAdapter } from "./email-validator";
+import validator from "validator";
+import { isMainThread } from "worker_threads";
 
-jest.mock('validator', ()=>({
-    isEmail (): boolean {
-        return true
-    }
-}))
+jest.mock("validator", () => ({
+  isEmail(): boolean {
+    return true;
+  },
+}));
 
 describe("Email validator adapter", () => {
-
   test("Should return false if validator returns false", () => {
     const sut = new EmailValidatorAdapter();
-    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
     const isValid = sut.isValid("invalid@mail.com"); //mocka um retorno falso para esta chamada
     expect(isValid).toBe(false);
   });
@@ -21,5 +20,12 @@ describe("Email validator adapter", () => {
     const sut = new EmailValidatorAdapter();
     const isValid = sut.isValid("valid@mail.com");
     expect(isValid).toBe(true);
+  });
+
+  test("Should call validator with correct param, email", () => {
+    const sut = new EmailValidatorAdapter();
+    const isEmailSpy = jest.spyOn(validator, "isEmail");
+    sut.isValid("any@mail.com");
+    expect(isEmailSpy).toHaveBeenCalledWith("any@mail.com");
   });
 });
