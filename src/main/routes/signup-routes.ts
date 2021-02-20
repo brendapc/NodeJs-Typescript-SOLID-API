6148df14d251ec431853949774bcd703306423af
Brenda Pereira <brendacamara@outlook.com>
2021-02-19 21:10:31 -0300

    feat: add express route adapter for factory use

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,9 +1,7 @@
+import { makeSignUpController } from "./../factories/signup";
+import { adaptRoute } from "./../adapters/express-route-adapter";
 import { Router } from "express";
 
 export default (router: Router): void => {
-  router.post("/signup", (req, res) => {
-    res.json({
-      ok: "ok",
-    });
-  });
+  router.post("/signup", adaptRoute(makeSignUpController()));
 };
