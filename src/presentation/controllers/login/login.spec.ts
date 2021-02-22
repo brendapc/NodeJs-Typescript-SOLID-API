92f9852a5593dd371e4fcbe546ca2db28d1ef9e3
Brenda Pereira <brendacamara@outlook.com>
2021-02-22 18:53:45 -0300

    test: ensure LoginController calls EmailValidator with correct email

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,30 +1,52 @@
+import { EmailValidator } from "./../../protocols/email-validator";
 import { MissingParamError } from "./../../protocols/errors/missing-param-error";
 import { badRequest } from "../../helpers/http-helper";
 import { LoginController } from "./login";
 
+interface SutTypes {
+  sut: LoginController;
+  emailValidatorStub: EmailValidator;
+}
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true;
+    }
+  }
+  return new EmailValidatorStub();
+};
+
+const makeSut = (): SutTypes => {
+  const emailValidatorStub = makeEmailValidator();
+  const sut = new LoginController(emailValidatorStub);
+  return {
+    sut,
+    emailValidatorStub,
+  };
+};
+
 describe("Login Controller", () => {
   test("should return 400 if no email is provided", async () => {
-    const sut = new LoginController();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         password: "any_password",
       },
     };
     const httpResponse = await sut.handle(httpRequest);
-    expect(httpResponse).toEqual(
-      badRequest(new MissingParamError("email"))
-    );
+    expect(httpResponse).toEqual(badRequest(new MissingParamError("email")));
   });
   test("should return 400 if no password is provided", async () => {
-    const sut = new LoginController();
+    const { sut, emailValidatorStub } = makeSut();
+    const isValidSpy = jest.spyOn(emailValidatorStub, "isValid");
     const httpRequest = {
       body: {
-        email: "any_email",
+        email: "any_email@mail.com",
+        password: "any_password",
       },
     };
-    const httpResponse = await sut.handle(httpRequest);
-    expect(httpResponse).toEqual(
-      badRequest(new MissingParamError("password"))
-    );
+    await sut.handle(httpRequest);
+    expect(isValidSpy).toHaveBeenCalledWith(httpRequest.body.email);
   });
 });
