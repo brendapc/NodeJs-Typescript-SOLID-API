03c35f0c36915b08882380042a2568e2f0b0acf2
Brenda Pereira <brendacamara@outlook.com>
2021-02-23 08:45:14 -0300

    feat: ensure auth middleware calls LoadAccountByToken if has token

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,12 +1,53 @@
+import { resolve } from "path";
+import { ILoadAccountByToken } from "./../../domain/usecases/load-accountby-token";
 import { badRequest } from "./../helpers/http-helper";
 import { AuthMiddleware } from "./auth-middleware";
 import { HttpRequest } from "./../protocols/http";
 import { AccessDeniedError } from "../errors/AccessDeniedError";
+import { AccountModel } from "../../domain/models/account";
 
 describe("Auth Middlware", () => {
   test("should return 403 if no token id found in headers", async () => {
-    const sut = new AuthMiddleware();
+    class LoadAccountByToken implements ILoadAccountByToken {
+      async load(accessToken: string, role?: string): Promise<AccountModel> {
+        const account = {
+          id: "id",
+          name: "name",
+          email: "email",
+          password: "password",
+        };
+        return new Promise((resolve) => resolve(account));
+      }
+    }
+
+    const loadAccountByTokenStub = new LoadAccountByToken();
+    const sut = new AuthMiddleware(loadAccountByTokenStub);
     const httpResponse = await sut.handle({});
     expect(httpResponse).toEqual(badRequest(new AccessDeniedError()));
+  });
+
+  test("should call LoadAccountByToken with correct accessToken", async () => {
+    class LoadAccountByToken implements ILoadAccountByToken {
+      async load(accessToken: string, role?: string): Promise<AccountModel> {
+        const account = {
+          id: "id",
+          name: "name",
+          email: "email",
+          password: "password",
+        };
+        return new Promise((resolve) => resolve(account));
+      }
+    }
+
+    const loadAccountByTokenStub = new LoadAccountByToken();
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, "load");
+    const sut = new AuthMiddleware(loadAccountByTokenStub);
+    const httpRequest = {
+      headers: {
+        "x-access-token": "any_token",
+      },
+    };
+    await sut.handle(httpRequest);
+    expect(loadSpy).toHaveBeenCalledWith(httpRequest.headers["x-access-token"]);
   });
 });
--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,10 +1,18 @@
+import { ILoadAccountByToken } from "../../domain/usecases/load-accountby-token";
 import { AccessDeniedError } from "../errors/AccessDeniedError";
 import { badRequest } from "./../helpers/http-helper";
 import { HttpResponse, HttpRequest } from "./../protocols/http";
 import { Middleware } from "./../protocols/middleware";
+
 export class AuthMiddleware implements Middleware {
-  async handle(HttpRequest: HttpRequest): Promise<HttpResponse> {
+  constructor(private readonly loadAccountByToken: ILoadAccountByToken) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const accessToken = httpRequest.headers?.["x-access-token"];
+    if (accessToken) {
+      await this.loadAccountByToken.load(accessToken);
+    }
     const error = badRequest(new AccessDeniedError());
-    return new Promise((resolve) => resolve(error));
+    return error;
   }
 }
