76fb796b4331ecb05f04a4bebd742d1107db5110
Brenda Pereira <brendacamara@outlook.com>
2021-03-11 21:20:24 -0300

    refactor: separe factories in folders

---
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -0,0 +1,16 @@
+import { makeDbAddAccount } from "./../../usecases/add-account/db-add-account-factory";
+import { makeDbAuthentication } from "./../../usecases/authentication/db-authentication-factory";
+import { SignUpController } from "../../../../presentation/controllers/signup/signup-controller";
+import { makeSignUpValidation } from "./signup-validation-factory";
+import { makeLogControllerDecorator } from "../../decorators/log-controller-decorator-factory";
+import { Controller } from "../../../../presentation/protocols";
+
+export const makeSignUpController = (): Controller => {
+  return makeLogControllerDecorator(
+    new SignUpController(
+      makeDbAddAccount(),
+      makeSignUpValidation(),
+      makeDbAuthentication()
+    )
+  );
+};
