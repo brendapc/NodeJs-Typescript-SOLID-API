3428ace96d88d56229bc38563470bb3abd09592f
Brenda Pereira <brendacamara@outlook.com>
2021-03-19 19:01:57 -0300

    feat: ensure LoadAccountByTokenStub is called with correct value

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,12 +1,21 @@
+import { LoadAccountByToken } from "./../../domain/usecases/load-account-by-token";
 import { forbbiden } from "./../helpers/http/http-helper";
 import { resolve } from "node:path";
 import { HttpRequest, HttpResponse } from "./../protocols/http";
 import { Middleware } from "./../protocols/middleware";
 import { AccessDeniedError } from "../errors";
 export class AuthMiddleware implements Middleware {
+  constructor(private readonly loadAccountByToken: LoadAccountByToken) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    return new Promise((resolve) =>
-      resolve(forbbiden(new AccessDeniedError()))
-    );
+    const accessToken = httpRequest.headers?.["x-access-token"];
+
+    if (accessToken) {
+      this.loadAccountByToken.load(accessToken);
+    } else {
+      return new Promise((resolve) =>
+        resolve(forbbiden(new AccessDeniedError()))
+      );
+    }
   }
 }
