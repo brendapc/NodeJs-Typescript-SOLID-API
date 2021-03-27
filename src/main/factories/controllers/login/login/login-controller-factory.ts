252a43c68ac5c8b3d606efe9039d394a731b1f8d
Brenda Pereira <brendacamara@outlook.com>
2021-03-27 18:03:13 -0300

    refactor: sepate entities by directories

---
+++ src/main/factories/controllers/login/login/login-controller-factory.ts
@@ -0,0 +1,12 @@
+import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory';
+import { makeDbAuthentication } from "../../../usecases/account/authentication/db-authentication-factory";
+import { LoginController } from "../../../../../presentation/controllers/login/login/login-controller";
+import { Controller } from "../../../../../presentation/protocols";
+import { makeLoginValidation } from "./login-validation-factory";
+
+export const makeLoginController = (): Controller => {
+  return makeLogControllerDecorator(new LoginController(
+    makeDbAuthentication(),
+    makeLoginValidation()
+  ));
+};
--- src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.spec.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.spec.ts
@@ -2,11 +2,11 @@ import {
   EmailValidation,
   RequiredFieldValidation,
   ValidationComposite,
-} from "../../../../validation/validators";
-import { Validation } from "../../../../presentation/protocols/validation";
+} from "../../../../../validation/validators";
+import { Validation } from "../../../../../presentation/protocols/validation";
 import { makeAddSurveyValidation } from "./add-survey-validation-factory";
 
-jest.mock("../../../../validation/validators/validation-composite");
+jest.mock("../../../../../validation/validators/validation-composite");
 
 describe("AddSurveyValidation Factory", () => {
   test("Should call ValidationComposite with all validations", () => {
