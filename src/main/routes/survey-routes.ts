62fa66f8b2c913c511f184a8d45800da7af6a9a7
Brenda Pereira <brendacamara@outlook.com>
2021-03-28 13:26:07 -0300

    feat: add middleware to survey route

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,8 +1,10 @@
-/* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from "express";
 import { adaptRoute } from "../adapters/express/express-route-adapter";
+import { adaptMiddleware } from "./../adapters/express/express-middleware-adapter";
+import { makeAuthMiddleware } from "./../factories/middlewares/auth-middleware-factory";
 import { makeAddSurveyController } from "../factories/controllers/survey/add-survey/add-survey-controller-factory";
 
 export default (router: Router): void => {
-  router.post("/surveys", adaptRoute(makeAddSurveyController()));
+  const adminRoute = adaptMiddleware(makeAuthMiddleware("admin"));
+  router.post("/surveys", adminRoute, adaptRoute(makeAddSurveyController()));
 };
