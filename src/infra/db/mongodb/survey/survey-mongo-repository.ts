fff4bd2246ecafa373cb8e15246632e6e83afbae
Brenda Pereira <brendacamara@outlook.com>
2021-04-08 18:22:40 -0300

    feat: ensure SurveysMongoRepository loads all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,17 @@
+import { SurveysModel } from './../../../../domain/models/survey';
+import { MongoHelper } from './../helpers/mongo-helper';
 import { AddSurveyModel } from "../../../../domain/usecases/add-survey";
-import { MongoHelper } from "../helpers/mongo-helper";
 import { AddSurveyRepository } from "./../../../../data/protocols/db/survey/add-survey-repository";
 
 export class SurveyMongoRepository implements AddSurveyRepository {
   async add(surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection("surveys");
     const result = await surveyCollection.insertOne(surveyData);
+  }
+
+  async loadAll(): Promise<SurveysModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveys: SurveysModel[] = await surveyCollection.find().toArray()
+    return surveys
   }
 }
