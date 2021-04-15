6d900f8fd43f182e0d9b489b43a597cf4166cf48
Brenda Pereira <brendacamara@outlook.com>
2021-04-14 22:31:14 -0300

    test: ensure SurveyResultMongoRepository updates result if its not new

--- src/infra/db/mongodb/survey/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-result/survey-result-mongo-repository.spec.ts
@@ -71,5 +71,26 @@ describe("Survey Mongo Repository", () => {
       expect(surveyResult.id).toBeTruthy()
       expect(surveyResult.answer).toBe(fakeSurvey.answers[0].answer)
     });
+    test('should update a survey result if it is not a new one', async () => {
+    const fakeAccount = await makeFakeAccount() 
+    const fakeSurvey = await makeFakeSurvey()
+    const res = await surveyResultCollection.insertOne({
+        surveyId: fakeSurvey.id,
+        accountId: fakeAccount.id,
+        answer: fakeSurvey.answers[0].answer,
+        date: new Date()
+    })
+    const sut = makeSut()
+    const surveyResult = await sut.save({
+        surveyId: fakeSurvey.id,
+        accountId: fakeAccount.id,
+        answer: fakeSurvey.answers[1].answer,
+        date: new Date()
+    })       
+    expect(surveyResult).toBeTruthy()
+    expect(surveyResult.id).toEqual(res.ops[0]._id)
+    expect(surveyResult.answer).toBe(fakeSurvey.answers[1].answer)
+    })
+    
  });
 });
