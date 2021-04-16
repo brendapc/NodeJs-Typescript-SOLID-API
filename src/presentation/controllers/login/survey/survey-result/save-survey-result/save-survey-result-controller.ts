d3370a76622ad909b2f9d8d14062ad5cc41fa139
Brenda Pereira <brendacamara@outlook.com>
2021-04-16 20:50:14 -0300

    feat: ensure SaveSurveyResultController throws if LoadSurveyById throws

--- src/presentation/controllers/login/survey/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/login/survey/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,3 +1,4 @@
+import { serverError } from './../../../../../helpers/http/http-helper';
 import { InvalidParamError } from './../../../../../errors/invalid-param-error';
 import { LoadSurveyById } from './../../../../../../domain/usecases/survey/load-survey-by-id';
 import { HttpRequest, HttpResponse } from '../../add-survey/add-survey-controller-protocols';
@@ -7,9 +8,14 @@ import { forbbiden } from '../../../../../helpers/http/http-helper';
 export class SaveSurveyResultController implements Controller{
     constructor(private readonly _loadSurveyById: LoadSurveyById){}
     async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-        const survey = await this._loadSurveyById.loadById(httpRequest.params.surveyId)
-        if(!survey) return forbbiden( new InvalidParamError('surveyId'))
-        return null
+        try{
+
+            const survey = await this._loadSurveyById.loadById(httpRequest.params.surveyId)
+            if(!survey) return forbbiden( new InvalidParamError('surveyId'))
+            return null
+        }catch(err){
+            return serverError(error)
+        }
     }
 
 }
