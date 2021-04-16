689a9d2f2d2d668ac753a35312e712ff4f194998
Brenda Pereira <brendacamara@outlook.com>
2021-04-16 20:39:25 -0300

    feat: ensure SaveSurveyResultController returns 403 if invalid survey is provided

--- src/presentation/controllers/login/survey/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/login/survey/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,11 +1,14 @@
+import { InvalidParamError } from './../../../../../errors/invalid-param-error';
 import { LoadSurveyById } from './../../../../../../domain/usecases/survey/load-survey-by-id';
 import { HttpRequest, HttpResponse } from '../../add-survey/add-survey-controller-protocols';
 import { Controller } from './../../../../../protocols/controller';
+import { forbbiden } from '../../../../../helpers/http/http-helper';
 
 export class SaveSurveyResultController implements Controller{
     constructor(private readonly _loadSurveyById: LoadSurveyById){}
     async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-        await this._loadSurveyById.loadById(httpRequest.params.surveyId)
+        const survey = await this._loadSurveyById.loadById(httpRequest.params.surveyId)
+        if(!survey) return forbbiden( new InvalidParamError('surveyId'))
         return null
     }
 
