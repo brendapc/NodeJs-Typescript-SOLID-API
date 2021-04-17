e28b96f6ba4a8565a34c3facc33da264fa5178d6
Brenda Pereira <brendacamara@outlook.com>
2021-04-16 21:15:43 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct value

--- src/presentation/controllers/login/survey/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/login/survey/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,3 +1,4 @@
+import { ISaveSurveyResult } from './../../../../../../domain/usecases/survey-result/save-survey-result';
 import { serverError } from './../../../../../helpers/http/http-helper';
 import { InvalidParamError } from './../../../../../errors/invalid-param-error';
 import { LoadSurveyById } from './../../../../../../domain/usecases/survey/load-survey-by-id';
@@ -6,11 +7,15 @@ import { Controller } from './../../../../../protocols/controller';
 import { forbbiden } from '../../../../../helpers/http/http-helper';
 
 export class SaveSurveyResultController implements Controller{
-    constructor(private readonly _loadSurveyById: LoadSurveyById){}
+    constructor(
+        private readonly _loadSurveyById: LoadSurveyById,
+        private readonly _saveSurveyResult: ISaveSurveyResult
+        ){}
     async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
         try{
             const { surveyId } = httpRequest.params
             const { answer }= httpRequest.body
+            const { accountId }= httpRequest
             const survey = await this._loadSurveyById.loadById(surveyId)
             if(survey){
                 const answers = survey.answers.map((answersList)=> answersList.answer)
@@ -20,7 +25,13 @@ export class SaveSurveyResultController implements Controller{
             }else{
                 return forbbiden( new InvalidParamError('surveyId'))
             }
-                return null
+            await this._saveSurveyResult.save({
+                accountId,
+                surveyId,
+                answer,
+                date: new Date()
+            })
+            return null
         }catch(err){
             return serverError(err)
         }
