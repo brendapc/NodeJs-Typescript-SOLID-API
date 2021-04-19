e7add0b3263895065c7d133e8ce383530d5bedb6
Brenda Pereira <brendacamara@outlook.com>
2021-04-18 21:26:11 -0300

    refactor: update inport

---
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -0,0 +1,64 @@
+import {
+  AddSurveyModel,
+  AddSurveyRepository,
+} from "../survey/add-survey/db-add-survey-protocols";
+import MockDate from "mockdate";
+import { DbAddSurvey } from "../survey/add-survey/db-add-survey";
+
+const makeFakeSurveyData = (): AddSurveyModel => ({
+  question: "any_question",
+  answers: [
+    {
+      image: "any_image",
+      answer: "any_answer",
+    },
+  ],
+  date: new Date(),
+});
+
+const makeAddSurveyRepository = (): AddSurveyRepository => {
+  class AddSurveyRepositoryStub implements AddSurveyRepository {
+    async add(surveyData: AddSurveyModel): Promise<void> {
+      return new Promise((resolve) => resolve());
+    }
+  }
+  return new AddSurveyRepositoryStub();
+};
+interface SutTypes {
+  sut: DbAddSurvey;
+  addSurveyRepositoryStub: AddSurveyRepository;
+}
+const makeSut = (): SutTypes => {
+  const addSurveyRepositoryStub = makeAddSurveyRepository();
+  const sut = new DbAddSurvey(addSurveyRepositoryStub);
+  return {
+    addSurveyRepositoryStub,
+    sut,
+  };
+};
+
+describe("DbAddSurvey Usecase", () => {
+  beforeAll(() => {
+    MockDate.set(new Date());
+  });
+
+  afterAll(() => {
+    MockDate.reset();
+  });
+
+  test("should call AddSurveyRepository with correct values", async () => {
+    const { sut, addSurveyRepositoryStub } = makeSut();
+    const addSpy = jest.spyOn(addSurveyRepositoryStub, "add");
+    const surveyData = makeFakeSurveyData();
+    await sut.add(surveyData);
+    expect(addSpy).toHaveBeenCalledWith(surveyData);
+  });
+  test("should throw if AddSurveyRepository throws", async () => {
+    const { sut, addSurveyRepositoryStub } = makeSut();
+    jest.spyOn(addSurveyRepositoryStub, "add").mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const promise = sut.add(makeFakeSurveyData());
+    await expect(promise).rejects.toThrow();
+  });
+});
